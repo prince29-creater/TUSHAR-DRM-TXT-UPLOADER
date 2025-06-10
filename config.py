bbf4cdca4dc1b18f4536b0bb7f745db5d8719b4c
prince29-creater <bhaduprince1@gmail.com>
2025-06-10 17:19:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.environ.get("API_ID", "24473318") #Replace With your api id
+API_ID = os.environ.get("API_ID", "21716150") #Replace With your api id
 API_HASH = os.environ.get("API_HASH", "63f3b7ddd0f6f8570dbd4fb8a24ae289") #Replace With your api hash
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8047811205:AAE5buzQp_WjF23nvWJLJTBJ5PPPTFLUr6o") #Replace With your bot token
